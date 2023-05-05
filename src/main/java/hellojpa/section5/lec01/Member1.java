0ff664286bba3102b1f206fe7fa9c557b8ef2266
amd <amd@devin.com>
2023-05-05 20:45:57 +0900

    섹션5(연관관계 매핑 기초) - 강의 1-5 (단방향 연관관계)
    Member 테이블과 Team 테이블을 따로 가져오는 설정.
    
    @ManyToOne(fetch = FetchType.LAZY)

--- src/main/java/hellojpa/section5/lec01/Member1.java
+++ src/main/java/hellojpa/section5/lec01/Member1.java
@@ -13,7 +13,7 @@ public class Member1 {
     @Column(name = "USERNAME")
     private String username;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "TEAM_ID")
     private Team1 team;
 
